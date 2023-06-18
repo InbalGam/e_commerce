21afb409f6d84ebb2fcf00512b77657d6f8634c6
InbalGam <inbalgam@post.bgu.ac.il>
2023-06-18 11:25:24 +0300

    started auth api

---
+++ server.js
@@ -0,0 +1,100 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const passport = require("passport");
+const session = require("express-session");
+const LocalStrategy = require("passport-local").Strategy;
+const store = new session.MemoryStore();
+const bcrypt = require("bcrypt");
+const {pool} = require('./server/db');
+
+module.exports = app;
+
+const PORT = process.env.PORT || 4001;
+
+app.use(morgan('short'));
+// middleware for handling CORS requests from index.html
+app.use(cors());
+
+
+// middleware for parsing request bodies here:
+app.use(bodyParser.json());
+
+
+// middlewares for authentication
+app.use(
+  session({
+    secret: "f4z4gs$Gcg",
+    cookie: { maxAge: 300000000, secure: false },
+    saveUninitialized: false,
+    resave: false,
+    store,
+  })
+);
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+const comparePasswords = async (password, hash) => {
+  try {
+    const matchFound = await bcrypt.compare(password, hash);
+    return matchFound;
+  } catch (err) {
+    console.log(err);
+  }
+  return false;
+};
+
+passport.use(
+  new LocalStrategy(async function (username, password, done) {
+    try {
+      const results = await pool.query('select * from users where username = $1', [username]);
+      if (results.rows.length === 0) {
+        return done(null, false);
+      }
+      const passwordCheck = await comparePasswords(password, results.rows[0].password);
+      if (!passwordCheck) {
+        return done(null, false);
+      }
+      return done(null, results.rows[0]);
+    }
+    catch (e) {
+      done(err);
+    }
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const results = await pool.query('select * from users where id = $1', [id]);
+    done(null, results.rows[0]);
+  } catch(e) {
+    done(err);
+  }
+});
+
+
+// Start auth router
+const authRouter = require('./server/auth_api');
+app.use('/', authRouter);
+
+// Start app routers
+const storeRouter = require('./server/store_api');
+app.use('/', storeRouter);
+
+
+// This conditional is here for testing purposes:
+if (!module.parent) { 
+  // start the server listening at PORT below:
+  app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+  });
+}
---
+++ server/auth_api.js
@@ -0,0 +1,113 @@
+const express = require('express');
+const authRouter = express.Router();
+const {pool} = require('./db');
+const passport = require("passport");
+const bcrypt = require("bcrypt");
+
+// Registering a user
+const passwordHash = async (password, saltRounds) => {
+    try {
+      const salt = await bcrypt.genSalt(saltRounds);
+      return await bcrypt.hash(password, salt);
+    } catch (err) {
+      console.log(err);
+    }
+    return null;
+};
+
+
+authRouter.post("/register", async (req, res, next) => {
+    const { username, password, nickname, firstName, lastName, address, phone } = req.body;
+    if (username === undefined || password === undefined || nickname === undefined
+        || firstName === undefined || lastName === undefined || address === undefined || phone === undefined) {
+        return res.status(400).json({ msg: 'All fields should be specified' });
+    };
+
+    if (password.length < 8) {
+        return res.status(400).json({ msg: 'Password needs to be at least 8 characters' });
+    }
+
+    if (username.length < 3) {
+        return res.status(400).json({ msg: 'Username needs to be at least 3 characters' });
+    }
+
+    if (nickname.length < 3) {
+        return res.status(400).json({ msg: 'Nickname needs to be at least 3 characters' });
+    }
+
+    try {
+        const check = await pool.query('select * from users where username = $1 or nickname = $2;', [username, nickname]);
+        if (check.rows.length > 0) {
+            return res.status(400).json({ msg: 'Username or Nickname already exist, choose differently' });
+        }
+        const hashedPassword = await passwordHash(password, 10);
+        const timestamp = new Date(Date.now());
+        await pool.query('insert into users (username, password, nickname, first_name, last_name, address, phone, created_at) values ($1, $2, $3, $4, $5, $6, $7, $8);', 
+        [username, hashedPassword, nickname, firstName, lastName, address, phone, timestamp]);
+        res.status(201).json({
+            msg: "Success creating user"
+        })
+    } catch (e) {
+        res.status(500).json({ msg: 'Could not create user' });
+    }
+});
+
+
+// Login a user
+authRouter.get("/login", (req, res) => {
+    res.status(401).json({msg: 'Authentication failed'});
+});
+
+
+// Get user profile page
+authRouter.get("/profile", async (req, res) => {
+    try {
+        const result = await pool.query('select * from users where id = $1', [req.user.id]);
+        res.status(200).json(result.rows);
+    } catch (e) {
+        res.status(500);
+    }
+});
+
+
+authRouter.post("/login",
+    passport.authenticate("local", { failureRedirect: "/login" }),
+    (req, res) => {
+      res.redirect("/profile");
+    }
+);
+
+
+// Logout user
+authRouter.get('/logout', function(req, res, next){
+    req.logout(function(err) {
+      if (err) { return next(err); }
+      //res.redirect('/');
+      res.status(200).json({msg: 'Successfully logged out'})
+    });
+});
+
+
+
+// Update user profile page
+authRouter.put('/profile', async (req, res, next) => { 
+    const { address, phone } = req.body;
+
+    if (address === undefined || phone === undefined) {
+        return res.status(400).json({ msg: 'All fields should be specified' });
+    };
+
+    try {
+        const check = await pool.query('select * from users where id = $1', [req.user.id]);
+        if (check.rows.length === 0) {
+            return res.status(400).json({ msg: 'User does not exist, please register first' });
+        }
+        const timestamp = new Date(Date.now());
+        await pool.query('update users set address = $2, phone = $3, modified_at = $4 where id = $1;', [req.user.id, address, phone, timestamp]);
+        res.status(200).json({ msg: 'Updated user' });
+    } catch(e) {
+        res.status(500);
+    }
+});
+
+module.exports = authRouter;
