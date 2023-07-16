721e0eb722d0998ac96df92645b3d24e4474f639
InbalGam <inbalgam@post.bgu.ac.il>
2023-07-16 14:33:08 +0300

    added more functionality

--- frontend/e-commerce/src/Components/Profile.js
+++ frontend/e-commerce/src/Components/Profile.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from 'react-redux';
 import {selectProfile, loadProfile} from '../store/profileSlice';
 import FadeLoader from 'react-spinners/FadeLoader';
@@ -9,18 +9,33 @@ function Profile() {
     const profile = useSelector(selectProfile);
     const { hasError, isLoading } = useSelector((state) => state.profile);
     const dispatch = useDispatch();
+    const [showForm, setShowForm] = useState(false);
 
 
     useEffect(() => {
         dispatch(loadProfile());
     }, []);
 
+
+    function showEditForm(e) {
+        setShowForm(!showForm);
+    };
+
     return (
-        <div> {hasError ? 'Could not fetch profile, try again' : (isLoading ? <FadeLoader color={'#3c0c21'} size={150} className='loader' /> : <div>
+        <div> 
+        {hasError ? 'Could not fetch profile, try again' : 
+        (isLoading ? <FadeLoader color={'#3c0c21'} size={150} className='loader' /> : 
+        <div>
             <h2>Welcome {profile[0].nickname}!</h2>
             <p>{profile[0].first_name} {profile[0].last_name}</p>
-            <p>{profile[0].address}<EditIcon className="editIcon"/></p>
-            <p>{profile[0].phone}<EditIcon className="editIcon"/></p>
+            <div className="editContainer">
+                <p>{profile[0].address}</p>
+                <button className='editIcon' onClick={showEditForm}><EditIcon /></button>
+            </div>
+            <div className="editContainer">
+                <p>{profile[0].phone}</p>
+                <button className='editIcon' onClick={showEditForm}><EditIcon /></button>
+            </div>
         </div>)}
         </div>
     );
