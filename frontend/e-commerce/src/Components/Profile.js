a2000cdede2e68008dd70aa3bf62d7a73052032b
InbalGam <inbalgam@post.bgu.ac.il>
2023-07-16 13:59:02 +0300

    added profile page

--- frontend/e-commerce/src/Components/Profile.js
+++ frontend/e-commerce/src/Components/Profile.js
@@ -1,3 +1,29 @@
-function Profile() {};
+import { useEffect } from "react";
+import { useSelector, useDispatch } from 'react-redux';
+import {selectProfile, loadProfile} from '../store/profileSlice';
+import FadeLoader from 'react-spinners/FadeLoader';
+import EditIcon from '@mui/icons-material/Edit';
+
+
+function Profile() {
+    const profile = useSelector(selectProfile);
+    const { hasError, isLoading } = useSelector((state) => state.profile);
+    const dispatch = useDispatch();
+
+
+    useEffect(() => {
+        dispatch(loadProfile());
+    }, []);
+
+    return (
+        <div> {hasError ? 'Could not fetch profile, try again' : (isLoading ? <FadeLoader color={'#3c0c21'} size={150} className='loader' /> : <div>
+            <h2>Welcome {profile[0].nickname}!</h2>
+            <p>{profile[0].first_name} {profile[0].last_name}</p>
+            <p>{profile[0].address}<EditIcon className="editIcon"/></p>
+            <p>{profile[0].phone}<EditIcon className="editIcon"/></p>
+        </div>)}
+        </div>
+    );
+};
 
 export default Profile;
---
+++ frontend/e-commerce/src/store/profileSlice.js
@@ -0,0 +1,46 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import {userProfile} from '../Api';
+import { useNavigate } from 'react-router-dom';
+
+
+export const loadProfile = createAsyncThunk(
+    '/profile',
+    async () => {
+        try {
+            const results = await userProfile();
+            const jsonData = await results.json();
+            return jsonData;
+        } catch (e) {
+            useNavigate('/error');
+        }
+    }
+);
+
+
+export const profileSlice = createSlice({
+    name: 'profile',
+    initialState: {
+        profile: [],
+        isLoading: false,
+        hasError: false
+    },
+    reducers: {},
+    extraReducers: {
+        [loadProfile.pending]: (state, action) => {
+            state.isLoading = true;
+            state.hasError = false;
+        },
+        [loadProfile.fulfilled]: (state, action) => {
+            state.profile = action.payload;
+            state.isLoading = false;
+            state.hasError = false;
+        },
+        [loadProfile.rejected]: (state, action) => {
+            state.isLoading = false;
+            state.hasError = true;
+        }
+    }
+});
+
+export const selectProfile = state => state.profile.profile;
+export default profileSlice.reducer;
