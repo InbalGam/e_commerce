7fa99ecdca7477fe22a0a950cf3650ca7cb9143b
InbalGam <inbalgam@post.bgu.ac.il>
2023-07-20 11:53:58 +0300

    adding category

--- frontend/e-commerce/src/Components/CategoryList.js
+++ frontend/e-commerce/src/Components/CategoryList.js
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import {selectCategories, loadCategories} from '../store/categorySlice';
 import FadeLoader from 'react-spinners/FadeLoader';
 import Category from './Category';
+import AddIcon from '@mui/icons-material/Add';
 
 
 function CategoryList() {
@@ -16,9 +17,14 @@ function CategoryList() {
 
     return (
         <div className="categoryContainer">
-            <ul>
-                {hasError ? 'Could not fetch categories, try again' : (isLoading ? <FadeLoader color={'#3c0c21'} size={150} className='loader' /> : categories.map((el, ind) => <Category el={el} ind={ind} />))}
-            </ul>
+            <div className="categories">
+                <div className="addCategory">
+                    <button className='add_category' onClick={showTrip}><AddIcon /></button>
+                </div>
+                <ul>
+                    {hasError ? 'Could not fetch categories, try again' : (isLoading ? <FadeLoader color={'#3c0c21'} size={150} className='loader' /> : categories.map((el, ind) => <Category el={el} ind={ind} />))}
+                </ul>
+            </div>
         </div>
     );
 };
