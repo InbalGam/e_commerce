13f65e6b3df2b950a7b1da6882cf8f256fd49962
InbalGam <inbalgam@post.bgu.ac.il>
2023-07-25 15:35:25 +0300

    fixed calculation

--- backend/server/store_api.js
+++ backend/server/store_api.js
@@ -372,7 +372,7 @@ storeRouter.put('/cart/:product_id', async (req, res, next) => {
         }
         if (product.rows[0].discount_percentage) {
             console.log('percentage');
-           calculatedPrice = (product.rows[0].price * quantity * (product.rows[0].discount_percentage/100));
+            calculatedPrice = (product.rows[0].price * quantity * (1 - (product.rows[0].discount_percentage/100))).toFixed(2);
         } else {
             console.log('no percentage');
             calculatedPrice = product.rows[0].price;
