61e78a582162c2e48edef7f650b553a4fb491c66
InbalGam <inbalgam@post.bgu.ac.il>
2023-07-27 11:18:22 +0300

    added ordersReducer

--- frontend/e-commerce/src/store/store.js
+++ frontend/e-commerce/src/store/store.js
@@ -3,12 +3,14 @@ import categoryReducer from './categorySlice';
 import profileReducer from './profileSlice';
 import productsReducer from './productSlice';
 import cartReducer from './cartSlice';
+import ordersReducer from './ordersSlice';
 
 export default configureStore({
   reducer: {
     categories: categoryReducer,
     profile: profileReducer,
     products: productsReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    orders: ordersReducer
   },
 });
