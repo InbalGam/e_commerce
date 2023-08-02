31d7328443d505557bbeee1ab9d89c03c7f6b4db
InbalGam <inbalgam@post.bgu.ac.il>
2023-08-02 16:04:18 +0300

    product css

--- frontend/e-commerce/src/Components/ProductsList.js
+++ frontend/e-commerce/src/Components/ProductsList.js
@@ -6,10 +6,10 @@ import AddIcon from '@mui/icons-material/Add';
 import {selectProfile} from '../store/profileSlice';
 import {loadImage, insertNewProduct} from '../Api';
 import { useParams, useNavigate } from 'react-router-dom';
-import Product from "./Product";
+//import Product from "./Product";
 import ProductAddUpdate from "./ProductAddUpdate";
 import styles from './Styles/ProductsList.css';
-
+import ProductCard from "./ProductCard";
 
 function ProductsList() {
     const products = useSelector(selectProducts);
@@ -59,27 +59,29 @@ function ProductsList() {
 
 
     return (
-        <div className="productsContainer">
+        <>
             {profile.is_admin ?
                 <div className="addProduct">
                     <button className='add_product' onClick={showAddProduct}><AddIcon /></button>
                     {showForm === false ? '' : <ProductAddUpdate onProductSubmit={onProductSubmit} />}
                 </div> : ''}
-            <div className="currentProducts">
-                <ul>
-                    {hasError ? 'Could not fetch products, try again' : (isLoading ? <FadeLoader color={'#3c0c21'} size={150} className='loader' /> : 
-                        products.length > 0 ? (products.map((el, ind) => el.is_archived ? '' : <Product el={el} ind={ind} admin={profile.is_admin} isArchived={false}/>)) : 'There are no products yet')}
-                </ul>
+            <div className="productsContainer">
+                <div className="products">
+                    <ul>
+                        {hasError ? 'Could not fetch products, try again' : (isLoading ? <FadeLoader color={'#3c0c21'} size={150} className='loader' /> :
+                            products.length > 0 ? (products.map((el, ind) => el.is_archived ? '' : <li key={ind}><ProductCard el={el} ind={ind} admin={profile.is_admin} isArchived={false} /></li>)) : 'There are no products yet')}
+                    </ul>
+                </div>
+                {profile.is_admin ?
+                    <div className="products">
+                        <p>Archived Products</p>
+                        <ul>
+                            {hasError ? 'Could not fetch products, try again' : (isLoading ? <FadeLoader color={'#3c0c21'} size={150} className='loader' /> :
+                                products.length > 0 ? (products.map((el, ind) => el.is_archived ? <li key={ind}><ProductCard el={el} ind={ind} admin={profile.is_admin} isArchived={true} /></li> : '')) : 'There are no products yet')}
+                        </ul>
+                    </div> : ''}
             </div>
-            {profile.is_admin ? 
-            <div className="archivedProducts">
-                <p>Archived Products</p>
-                <ul>
-                    {hasError ? 'Could not fetch products, try again' : (isLoading ? <FadeLoader color={'#3c0c21'} size={150} className='loader' /> : 
-                       products.length > 0 ? (products.map((el, ind) => el.is_archived ? <Product el={el} ind={ind} admin={profile.is_admin} isArchived={true}/> : '' )) : 'There are no products yet')}
-                </ul>
-            </div> : '' }
-        </div>
+        </>
     );
 };
 
