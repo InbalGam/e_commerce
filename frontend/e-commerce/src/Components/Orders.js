902c012e33979908c7293f38063db3ae76e7b811
InbalGam <inbalgam@post.bgu.ac.il>
2023-08-03 12:12:58 +0300

    turned to a card

--- frontend/e-commerce/src/Components/Orders.js
+++ frontend/e-commerce/src/Components/Orders.js
@@ -1,18 +1,39 @@
 import { Link} from 'react-router-dom';
 import dateFormat, { masks } from "dateformat";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import styles from './Styles/Orders.css';
 
 
 function Orders(props) {
 
     return (
-        <li key={props.el.ind}>
-           <Link to={`/myOrders/${props.el.orderId}`} className={'orderLink'}><div>
-                <p>Order id {props.el.orderId} submitted on {dateFormat(new Date(props.el.createdAt), "mmmm dS, yyyy")}</p>
-                <p>Total of {props.el.orderTotal.toFixed(2)}$</p>
-                <p>Shipping address {props.el.shippedTo}</p>
-                <p>Contact number {props.el.contactPhone}</p>
-            </div></Link>
-        </li>
+        <>
+            <Link to={`/myOrders/${props.el.orderId}`} className={'orderLink'}>
+                <Card sx={{ minWidth: 375 }} className='orderCard'>
+                    <CardContent>
+                        <Typography sx={{ fontSize: 24 }} gutterBottom className='orderDataId'>
+                            Order id {props.el.orderId}
+                        </Typography>
+                        <Typography sx={{ fontSize: 24 }} gutterBottom className='orderData'>
+                            submitted on {dateFormat(new Date(props.el.createdAt), "mmmm dS, yyyy")}
+                        </Typography>
+                        <Typography sx={{ fontSize: 24 }} gutterBottom className='orderData'>
+                            Total of {props.el.orderTotal.toFixed(2)}$
+                        </Typography>
+                        <Typography sx={{ fontSize: 24 }} gutterBottom className='orderData'>
+                            Shipping address {props.el.shippedTo}
+                        </Typography>
+                        <Typography sx={{ fontSize: 24 }} gutterBottom className='orderData'>
+                            Contact number {props.el.contactPhone}
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </Link>
+        </>
     );
 };
 
